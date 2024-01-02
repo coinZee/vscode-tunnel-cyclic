930dd7a4c0fb742f286e0b88d36353456bb47e26
Nick <coinz1112@gmail.com>
2024-01-02 23:57:21 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import subprocess
 
+subprocess.run(["/bin/sh", "run.sh"])
+
+
 #gr = "python gr.py"
-main = "python app.py"
+#main = "python app.py"
 
 #grpr = subprocess.Popen(gr, shell=True)
-mainpr = subprocess.Popen(main, shell=True)
+#mainpr = subprocess.Popen(main, shell=True)
 
 #grpr.wait()
-mainpr.wait()
+#mainpr.wait()
