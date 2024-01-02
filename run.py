0e9df5409d66b066dfcece27270e7acd5ebe20f3
Nick <coinz1112@gmail.com>
2024-01-02 23:55:44 +0630

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import subprocess
+
+gr = "python gr.py"
+main = "python app.py"
+
+grpr = subprocess.Popen(gr, shell=True)
+mainpr = subprocess.Popen(main, shell=True)
+
+grpr.wait()
+mainpr.wait()
